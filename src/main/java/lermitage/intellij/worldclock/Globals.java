b949a39a546236e07abd4ffaddf50ef2dbb67f24
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2021-09-13 00:20:23 +0200

    fix widgets ordering

--- src/main/java/lermitage/intellij/worldclock/Globals.java
+++ src/main/java/lermitage/intellij/worldclock/Globals.java
@@ -1,10 +1,7 @@
 package lermitage.intellij.worldclock;
 
-import lermitage.intellij.worldclock.statusbar.ClockWidgetFactory;
-import lermitage.intellij.worldclock.statusbar.ClockWidgetFactory2;
-
 public class Globals {
 
-    public static final String WIDGET_ID = ClockWidgetFactory.class.getName();
-    public static final String WIDGET_ID_2 = ClockWidgetFactory2.class.getName();
+    public static final String WIDGET_ID = "lermitage-worldclock-one";
+    public static final String WIDGET_ID_2 = "lermitage-worldclock-two";
 }
