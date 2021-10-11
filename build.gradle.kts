352c31722c6500737cbb16385d3e128e6bfeea77
Jonathan Lermitage <jonathan.lermitage@gmail.com>
2021-10-11 22:03:37 +0200

    project structure improvements

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     id("java")
     id("idea")
-    id("org.jetbrains.intellij") version "1.1.6" // https://github.com/JetBrains/gradle-intellij-plugin
+    id("org.jetbrains.intellij") version "1.2.0" // https://github.com/JetBrains/gradle-intellij-plugin and https://lp.jetbrains.com/gradle-intellij-plugin/
+    id("com.github.ben-manes.versions") version "0.39.0" // https://github.com/ben-manes/gradle-versions-plugin
 }
 
 // Import variables from gradle.properties file
@@ -23,15 +26,11 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
-}
-
 intellij {
     downloadSources.set(pluginDownloadIdeaSources.toBoolean() && !inCI)
+    instrumentCode.set(pluginInstrumentPluginCode.toBoolean())
     pluginName.set("WorldClock")
-    sandboxDir.set("${rootProject.projectDir}/.idea-sandbox/${pluginIdeaVersion}")
+    sandboxDir.set("${rootProject.projectDir}/.idea-sandbox/${shortIdeVersion(pluginIdeaVersion)}")
     updateSinceUntilBuild.set(false)
     version.set(pluginIdeaVersion)
 }
@@ -42,14 +41,51 @@ tasks {
         targetCompatibility = pluginJavaVersion
         options.compilerArgs = listOf("-Xlint:deprecation")
     }
-    withType<Test> {
-        useJUnitPlatform()
+    withType<DependencyUpdatesTask> {
+        checkForGradleUpdate = true
+        gradleReleaseChannel = "current"
+        outputFormatter = "plain"
+        outputDir = "build"
+        reportfileName = "dependencyUpdatesReport"
+        revision = "release"
+        resolutionStrategy {
+            componentSelection {
+                all {
+                    if (isNonStable(candidate.version)) {
+                        logger.debug(" - [ ] ${candidate.module}:${candidate.version} candidate rejected")
+                        reject("Not stable")
+                    } else {
+                        logger.debug(" - [X] ${candidate.module}:${candidate.version} candidate accepted")
+                    }
+                }
+            }
+        }
     }
     runIde {
         jvmArgs = listOf("-Xms768m", "-Xmx2048m", "--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
     }
     buildSearchableOptions {
-        enabled = pluginEnableBuildSearchableOptions.toBoolean()
-        jvmArgs = listOf("--add-exports", "java.base/jdk.internal.vm=ALL-UNNAMED")
+        enabled = false
+    }
+}
+
+fun isNonStable(version: String): Boolean {
+    if (listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().endsWith(it) }) {
+        return false
+    }
+    return listOf("alpha", "Alpha", "ALPHA", "b", "beta", "Beta", "BETA", "rc", "RC", "M", "EA", "pr", "atlassian").any {
+        "(?i).*[.-]${it}[.\\d-]*$".toRegex().matches(version)
+    }
+}
+
+/** Return an IDE version string without the optional PATCH number.
+ * In other words, replace IDE-MAJOR-MINOR(-PATCH) by IDE-MAJOR-MINOR. */
+fun shortIdeVersion(version: String): String {
+    val matcher = Regex("[A-Za-z]+[\\-]?[0-9]+[\\.]{1}[0-9]+")
+    return try {
+        matcher.findAll(version).map { it.value }.toList()[0]
+    } catch (e: Exception) {
+        logger.warn("Failed to shorten IDE version $version", e)
+        version
     }
 }
